481877cc975071a29bc87275939a49c565b45eac
anurag-coinshred <anurag@coinshred.com>
2022-02-11 22:56:23 +0530

    updating the backend url

--- backendUrl.js
+++ backendUrl.js
@@ -1,3 +1,4 @@
 export const config = {
-  backend_url: "http://localhost:4000",
+  // backend_url: "http://localhost:4000",
+  backend_url: "http://cryptobackend-env.eba-hmpxxwbg.us-east-1.elasticbeanstalk.com/",
 }
